cf97ccfec45cdf95107d41b6b00a31cce14ea426
yeahservice <dom.augustin@gmx.at>
2018-04-05 22:46:26 +0200

    initial commit

---
+++ src/main/java/at/yeahservice/javaeetest/JAXRSConfiguration.java
@@ -0,0 +1,19 @@
+package at.yeahservice.javaeetest;
+
+import at.yeahservice.javaeetest.users.boundary.UsersManager;
+import javax.inject.Inject;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures a JAX-RS endpoint. Delete this class, if you are not exposing
+ * JAX-RS resources in your application.
+ *
+ * @author airhacks.com
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+    @Inject
+    UsersManager um;
+}
